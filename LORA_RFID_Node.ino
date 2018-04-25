757542a793f4f7de89970df7f0ad41ae9e5f410e
Marcel Wagner <marcel_wagner_@arcor.de>
2018-04-25 22:56:54 +0200

    Added TFT Display
    TFT Display shows the text value of Block 1 which is read by the RFID reader. Both devices are connected via the SPI bus and only require seperate Reset and CS signals!

--- LORA_RFID_Node.ino
+++ LORA_RFID_Node.ino
@@ -1,33 +1,61 @@
  /*
+ * Connection of RFID Reader:
  * -----------------------------------------------------------------------------------------
  *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
  *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
  * Signal      Pin          Pin           Pin       Pin        Pin              Pin
  * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
+ * RST/Reset   RST          9             5         D7         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D2         10               10
  * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
  * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
  * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ * -----------------------------------------------------------------------------------------
+ * 
+ * 
+ * Connection of 1.8" TFT Display
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * Reset                                            D9
+ * CS                                               D2
+ * MOSI                                             D11
+ * SCK                                              D13
+ * A0/DC                                            D8
+ * -----------------------------------------------------------------------------------------
 */
 
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Adafruit_ST7735.h> 
+#include <Adafruit_GFX.h>
 
-#define RST_PIN         9           // Configurable, see typical pin layout above
-#define SS_PIN          10          // Needs to be changed when RFM95 needs to be connected
+#define RST_PIN         7          // Configurable, see typical pin layout above
+#define SS_PIN          2          // Needs to be changed when RFM95 needs to be connected
+
+#define TFT_CS          3          // TFT Chip Select
+#define TFT_RST         9          // Reset for all Devices
+#define TFT_DC          8              
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST); // Create TFT instance
 
 void setup() {
   Serial.begin(9600);        // Initialize serial communications with the PC
   SPI.begin();               // Init SPI bus
   mfrc522.PCD_Init();        // Init MFRC522 card
   Serial.println(F("READ BLOCK 1 INFO:"));
+
+  tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
+  tft.fillScreen(ST7735_BLACK);
+  tft.setRotation(1);
+  draw("", true);
 }
 
 void loop() {
-
   // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
   MFRC522::MIFARE_Key key;
   for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
@@ -61,8 +89,8 @@ void loop() {
 
   Serial.print(F("Block 1 Info: "));
 
-  byte buffer1[18];
-
+  byte buffer1[16];
+  String buff = "";
   block = 1;
   len = 18;
 
@@ -85,13 +113,38 @@ void loop() {
   for (uint8_t i = 0; i < 16; i++)
   {
     Serial.write(buffer1[i]);
+    if(buffer1[i] != 32 && buffer1[i] != 0){
+      buff += char(buffer1[i]);
+    }
   }
   Serial.print(" ");
   
   Serial.println("End Reading!");
-
-  delay(1000); //change value if you want to read cards faster
-
+  
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
+  draw(buff, false);
 }
+
+
+void draw(String s, boolean access){
+  tft.fillScreen(ST7735_BLACK);
+  tft.setTextSize(2);
+  tft.setCursor(0, 0);
+  tft.println("Key Code:");
+  if(s.length() > 0){
+    tft.println(s);
+    tft.println(" ");
+    if(access){
+      tft.setTextColor(ST7735_GREEN);
+      tft.println("Access ok!");
+      tft.setTextColor(ST7735_WHITE);
+    }else if(! access){
+      tft.setTextColor(ST7735_RED);
+      tft.println("Access error!");
+      tft.setTextColor(ST7735_WHITE);
+    }
+  }
+  
+}
+
